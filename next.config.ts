ae4dea76055652d5c71c593257133cfca34d544d
pareenita876 <vanur.copilot@gmail.com>
2026-06-13 11:06:06 +0530

    Update Next.js version

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
     // Allow loading images from the project's S3 bucket and common CDNs
-    domains: [
-      'chihili-bucket.s3.ap-south-1.amazonaws.com',
-      'cdn.example.com'
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "chihili-bucket.s3.ap-south-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.example.com",
+      },
     ],
   },
 };
